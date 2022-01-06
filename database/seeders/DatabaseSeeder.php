ed150a1c31a06b7635f25e0133c9effe4ebd37a1
Flávio Paixão <flavio_pc@outlook.com>
2022-01-06 09:29:03 -0300

    add seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $tags = Tag::factory()->count(100)->create();
+        $tags = $tags->modelKeys();
+        $products = Product::factory(50)->create();
+        foreach ($products as $product) {
+            $rand = rand(1, 3);
+            $val = array();
+            for ($i = 0; $i < $rand; $i++) {
+                $v = \rand(0, 99);
+                $val[] = $tags[$v];
+            }
+
+            $product->tags()->sync($val);
+        }
         // \App\Models\User::factory(10)->create();
     }
 }
